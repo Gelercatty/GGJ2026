d2b91c02cdc5f24583145329a56272c081970aa1
GelerCAT <135972531+Gelercatty@users.noreply.github.com>
2026-01-30 20:36:29 +0800

    添加架构

---
+++ Assets/Architecture.cs
@@ -0,0 +1,12 @@
+using QFramework;
+namespace DefaultNamespace
+{
+    public class ConterApp : Architecture<ConterApp> 
+    {
+        // 在这里注册Model
+        protected override void Init()
+        {
+            // this.RegisterModel<ICounterModel>(new CounterModel());
+        }
+    }
+}
