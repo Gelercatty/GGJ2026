64ca3faaa081c2ed26c06d3d243adc62c82a53d8
chaos <1215599200ir@gmail.com>
2026-01-31 22:28:31 +0800

    22:28

--- Assets/Controller/Stage1UI/CluePanelController.cs
+++ Assets/Controller/Stage1UI/CluePanelController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 using QFramework;
 using TMPro;
 using GGJ2026;
+using UnityEngine.EventSystems;
 using System;
 
-public class CluePanelController : MonoBehaviour, IController
+public class CluePanelController : MonoBehaviour, IController,IPointerClickHandler
 {
     public TextMeshProUGUI textMeshProUGUI;
     private void Awake()
@@ -23,7 +24,17 @@ public class CluePanelController : MonoBehaviour, IController
         textMeshProUGUI.text = GameApp.Interface.GetModel<UIStage_1_Model>().ClueText.Value;
     }
 
-   
+   public void OnPointerClick(PointerEventData eventData)
+    {
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(textMeshProUGUI, eventData.position, eventData.pressEventCamera);
+        if (linkIndex != -1)
+        {
+            TMP_LinkInfo linkInfo = textMeshProUGUI.textInfo.linkInfo[linkIndex];
+            string linkId = linkInfo.GetLinkID();
+            Debug.Log("点击了链接: " + linkId);
+            // 在这里处理链接点击事件，比如根据linkId执行不同的操作
+        }
+    }
 
     public IArchitecture GetArchitecture() => GameApp.Interface;
     // Start is called before the first frame update
