ed4064f31792703fa4e723c01d1cab69a7b71180
chaos <1215599200ir@gmail.com>
2026-01-31 23:26:18 +0800

    可以点击超链接(线索)

--- Assets/Controller/NotePanelController.cs
+++ Assets/Controller/NotePanelController.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using QFramework;
+using TMPro;
+using GGJ2026;
 
-public class NotePanelController : MonoBehaviour
+public class NotePanelController : MonoBehaviour, IController
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI noteText;
+    
+    public IArchitecture GetArchitecture() => GameApp.Interface;
+    
+    private void Start()
     {
+        // 确保 TextMeshPro 组件已赋值
+        if (noteText == null)
+            noteText = GetComponent<TextMeshProUGUI>();
         
+        // 监听 ClueTextChangedEvent
+        this.RegisterEvent<ClueTextChangedEvent>(OnClueTextChanged)
+            .UnRegisterWhenGameObjectDestroyed(gameObject);
+        
+        // 初始化显示已收集的线索文本
+        UpdateNoteDisplay();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void OnClueTextChanged(ClueTextChangedEvent e)
     {
+        // 只有当是新收集的线索时才更新显示
+        if (e.IsNewCollection)
+        {
+            UpdateNoteDisplay();
+        }
+    }
+    
+    private void UpdateNoteDisplay()
+    {
+        var stage1Model = this.GetModel<UIStage_1_Model>();
         
+        if (noteText != null && stage1Model.CollectedClueTexts.Count > 0)
+        {
+            // 将收集到的线索文本按行显示
+            noteText.text = string.Join("\n", stage1Model.CollectedClueTexts);
+            Debug.Log($"笔记面板更新，显示 {stage1Model.CollectedClueTexts.Count} 条线索");
+        }
+        else if (noteText != null)
+        {
+            noteText.text = "暂无收集的线索";
+        }
     }
 }
